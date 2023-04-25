84d7fd6c23970103b1289cdd791a23c676ea8424
Irfan <irfanimanabub@gmail.com>
2023-04-25 09:14:11 +0800

    new cahnges

--- client/src/app/components/username.component.ts
+++ client/src/app/components/username.component.ts
@@ -5,7 +5,7 @@ import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { SongService } from '../song-service';
-import { Artist } from '../models';
+import { Artist, Albums } from '../models';
 
 @Component({
   selector: 'app-username',
@@ -13,55 +13,18 @@ import { Artist } from '../models';
   styleUrls: ['./username.component.css']
 })
 export class UsernameComponent implements OnInit{
-  
-  searchForm!: FormGroup
-  searchResult: any
 
   constructor(private router: Router, private http: HttpClient, private fb: FormBuilder, private songService: SongService) {}
 
-  ngOnInit(): void {
+  searchForm!: FormGroup
 
+
+  ngOnInit(): void {
+    
   }
 
   search() {
-    this.songService.get
 
   }
 
-  private createForm(): FormGroup {
-    return this.fb.group({
-      username: this.fb.control<String>(''),
-      artistInput: this.fb.control<String>('', [ Validators.required ])
-    })
-  }
 }
-
-
-// export class SearchComponent implements OnInit {
-
-//   @Output()
-//   onSearch = new Subject<SearchCriteria>()
-
-//   searchForm!: FormGroup
-
-//   constructor(private fb: FormBuilder) { }
-
-//   ngOnInit(): void {
-//     this.searchForm = this.createForm()
-//   }
-
-//   processForm() {
-//     const searchCriterial: SearchCriteria = this.searchForm.value
-//     searchCriterial.count = parseInt(this.searchForm.value.count)
-//     this.onSearch.next(searchCriterial)
-//     this.searchForm = this.createForm()
-//   }
-
-//   private createForm(): FormGroup {
-//     return this.fb.group({
-//       text: this.fb.control<string>('', [ Validators.required ]),
-//       count: this.fb.control<number>(5)
-//     })
-//   }
-
-
